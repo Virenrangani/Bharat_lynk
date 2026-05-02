aa5e4f43e9371b847c01e20227dc101bfbe82234
Virenrangani <virenrangani7@gmail.com>
2026-05-02 09:27:28 +0530

    feat: add country model class to define parameter

---
+++ lib/feature/login/presenatation/widget/country_model.dart
@@ -0,0 +1,11 @@
+class Country {
+  final String name;
+  final String code;
+  final String flag; // emoji OR asset path
+
+  Country({
+    required this.name,
+    required this.code,
+    required this.flag,
+  });
+}
