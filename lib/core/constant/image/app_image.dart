aac80a801b82c8494f6961d32de893a9e9ac1611
Virenrangani <virenrangani7@gmail.com>
2026-05-02 08:06:22 +0530

    feat: add the image path

---
+++ lib/core/constant/image/app_image.dart
@@ -0,0 +1,10 @@
+class AppImage {
+  static const String onBoarding1=
+      'asset/image/3ec5f69c465074646dd6333976266ecde7cb1a85.jpg';
+
+  static const String onBoarding2=
+      'asset/image/bf6b1bc63a9923a91c0ce51bd48ec44c90075289.jpg';
+
+  static const String onBoarding3=
+      'asset/image/3ec5f69c465074646dd6333976266ecde7cb1a85.jpg';
+}
